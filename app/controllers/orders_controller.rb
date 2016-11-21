4043d71b72774e7f578c4cde62d61a7ceb252fc6
Pavlo <p.korenev@voroninstudio.eu>
2016-11-21 20:12:08 +0200

    fix prev commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
     order_params[:locale] = params[:locale]
     order_params[:referrer] = request.referrer
     order_params[:session_id] = session.id
-    equal_params = [:user_position, :name, :email, :phone, :message, :locale, :referrer, :session_id].map{|k| [k, order_params[k]] }
+    equal_params = Hash[[:user_position, :name, :email, :phone, :message, :locale, :referrer, :session_id].map{|k| [k, order_params[k]] }]
     if Order.where(equal_params).empty?
       @order = Order.create(order_params)
     end  
