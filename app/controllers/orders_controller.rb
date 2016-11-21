561726fa5018a6c32ecd3b1976ab39452e5f0708
Pavlo <p.korenev@voroninstudio.eu>
2016-11-21 20:09:20 +0200

    Add check for order duplication

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,11 @@ class OrdersController < ApplicationController
     order_params[:locale] = params[:locale]
     order_params[:referrer] = request.referrer
     order_params[:session_id] = session.id
-    @order = Order.create(params[:order])
+    equal_params = [:user_position, :name, :email, :phone, :message, :locale, :referrer, :session_id].map{|k| [k, order_params[k]] }
+    if Order.where(equal_params).empty?
+      @order = Order.create(order_params)
+    end  
+    
     ApplicationMailer.new_order(@order).deliver
 
 
