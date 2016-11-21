2996e09d4f0ea3495185340cf0fd516983a1d2de
Pavlo <p.korenev@voroninstudio.eu>
2016-11-21 20:14:49 +0200

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,14 +5,16 @@ class OrdersController < ApplicationController
     order_params[:referrer] = request.referrer
     order_params[:session_id] = session.id
     equal_params = Hash[[:user_position, :name, :email, :phone, :message, :locale, :referrer, :session_id].map{|k| [k, order_params[k]] }]
+    
     if Order.where(equal_params).empty?
       @order = Order.create(order_params)
+      ApplicationMailer.new_order(@order).deliver
     end  
     
-    ApplicationMailer.new_order(@order).deliver
+    
 
 
 
-    render inline: "", status: 201
+    render inline: "", status: @order ? 201 : 200
   end
 end
